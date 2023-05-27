2ad1a5ea3ab1b55aa94a99910ca295e18b5bbba2
PedroFrancholi <francholidearaujo@gmail.com>
2023-05-27 18:27:22 -0300

    Trabalho Back-End

--- index.js
+++ index.js
@@ -1,48 +1,87 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
 
-const port = 8080;
-
-let Receita = [
-{   
-    "id"            : 1,
-    "name"          : "Cuca Maluca",
-    "description"   : "Pão com massa de canela",
-    "preparation"   : "Bater a massa até ficar homogênea"
-},
-{   
-    "id"            : 2,
-    "name"          : "Pão",
-    "description"   : "Pão caseiro",
-    "preparation"   : "Bater a massa, assar e servir"
-},
-{   
-    "id"            : 3,
-    "name"          : "Pavê",
-    "description"   : "Bolacha com creme e chocolate",
-    "preparation"   : "Colocar a bolacha na forma e adicionar creme e chocolate"
-}
-]
-
-
+let receita = [
+    {   
+        id                : 1,
+        name              : "Cuca Maluca",
+        description       : "Pão com massa de canela",
+        timePreparation   : "30 minutes"
+    },
+    {   
+        id                : 2,
+        name              : "Pão",
+        description       : "Pão caseiro",
+        timePreparation   : "1 hour"
+    },
+    {   
+        id                : 3,
+        name              : "Pavê",
+        description       : "Bolacha com creme e chocolate",
+        timePreparation   : "45 minutes"
+    }
+];
 
 server.get("/recipe",(req,res)=>{
     res.json({
-        Receita
+        receita
     })
 })
 
-server.get("/recipe/:id",(req,res)=>{
+server.get("/recipe/:id", (req,res)=>{
     const id = Number(req.params.id);
-    const action = Receita.find((action)=>{
-        return action.id===id;
+    const rec = receita.find((rec)=>{
+        return rec.id===id;
     });
-    
     res.json({
-        Receita
+        rec
     })
 })
+
+server.post("/recipe", (req,res)=>{
+    const newReceita = {
+        id                : receita.length+1,
+        name              : req.body.name,
+        description       : req.body.description,
+        timePreparation   : req.body.timePreparation
+    };
+    receita.push(newReceita);
+    res.json({
+        receita: newReceita
+    });
+});
+
+server.put("/recipe/:id", (req,res)=>{
+    const id = Number(req.params.id)
+    const change = receita.find((change)=>{
+        return change.id===id
+    });
+    
+    if(!receita) {
+        return res.status(404).send("receita not found");
+    }
+
+    change.name               = req.body.name;
+    change.description        = req.body.description;
+    change.timePreparation    = req.body.timePreparation;
+    
+    res.json({
+        change
+    })
+})
+
+server.delete("/recipe/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    receita = receita.filter((rec)=>{
+        return rec.id!=id;
+    })
+    
+    res.status(204).send()
+})
+
+const port = 8080;
 
 server.listen(port,()=>{
     console.log(`Server running on port ${port}`)
